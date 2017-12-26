1146e498db56736396a9eba59263608952b927a9
Michael Noronha <michaeltnoronha@gmail.com>
2017-12-26 01:08:35 -0500

    Finish lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,5 +1,6 @@
 
-enum Token {
+#[derive(Debug)]
+pub enum Token {
     Lambda,
     Dot,
     LParen,
@@ -8,12 +9,96 @@ enum Token {
     Atom(String),
 }
 
+pub struct Lexer {
+    input: Vec<char>,
+    ind: usize,
+}
+
+impl Lexer {
+    pub fn new(input: String) -> Lexer {
+        Lexer {
+            input: input.chars().filter(|x| !x.is_whitespace()).collect(),
+            ind : 0,
+        }
+    }
+
+    fn advance(&mut self) {
+        self.ind += 1;
+    }
+
+    fn get_token (&mut self) -> Option<Token> {
+        if self.ind >= self.input.len() {
+            return None
+        }
+
+        match self.input[self.ind] {
+            'λ'| '\\' => {
+                self.advance();
+                Some(Token::Lambda)
+            },
+            '.' => {
+                self.advance();
+                Some(Token::Dot)
+            },
+            '(' => {
+                self.advance();
+                Some(Token::LParen)
+            }
+            ')' => {
+                self.advance();
+                Some(Token::RParen)
+            }
+            '\0' => {
+                self.advance();
+                Some(Token::EOF)
+            }
+            ch => {
+                if !ch.is_alphabetic() && !ch.is_lowercase() {
+                    panic!("Malformed identifier");
+                }
+                self.advance();
+
+                let mut name = ch.to_string();
+                let input_len = self.input.len();
+                while self.ind < input_len {
+                    let ch = self.input[self.ind];
+                    if !ch.is_alphabetic() {
+                        break;
+                    }
+
+                    name.push(ch);
+                    self.advance();
+                }
+
+                Some(Token::Atom(name))
+            }
+        }
+    }
+
+    pub fn lex(&mut self) -> Vec<Token> {
+        let mut v: Vec<Token> = Vec::new();
+
+        while let Some(t) = self.get_token() {
+            v.push(t);
+        }
+        v.push(Token::EOF);
+
+        v
+    }
+}
+
 #[cfg(test)]
 mod tests {
 
-    #[test]
+    use super::*;
 
+    #[test]
     fn test_test() {
         assert_eq!(1+1, 2);
+    }
+
+    #[test]
+    fn test_equality() {
+        assert_eq!(Token::Lambda, Token::Lambda);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 mod lexer;
 
 fn main() {
+    let mut lexer = lexer::Lexer::new(String::from("((λy.λx.(y x)) (λx.x x)) y"));
+    let tokens = lexer.lex();
+    for tok in tokens {
+        println!("{:?}",tok);
+    }
     println!("Hello, world!");
 }
