6fa81e806177566778d731758e3dcb7a6dc7110e
Michael Noronha <michaeltnoronha@gmail.com>
2017-12-25 21:17:17 -0500

    Add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# tangerine
+
+This is a port of [fuchsia](https://github.com/mtn/fuchsia) to Rust, made so I can try out Rust as I learn it.
---
+++ src/lexer.rs
@@ -0,0 +1,19 @@
+
+enum Token {
+    Lambda,
+    Dot,
+    LParen,
+    RParen,
+    EOF,
+    Atom(String),
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+
+    fn test_test() {
+        assert_eq!(1+1, 2);
+    }
+}
